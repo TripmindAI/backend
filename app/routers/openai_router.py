33e902c5dd5cc5e4c06f5e70c61669dc7a8bcf74
Chong Wen <chongwen107@gmail.com>
2024-02-08 20:49:35 -0600

    add router files corresponding two service

---
+++ app/routers/openai_router.py
@@ -0,0 +1,16 @@
+# app/routers/openai_router.py
+from fastapi import APIRouter, HTTPException
+from ..services.openai_chat_service import fetch_recommedations
+from app.models import RecommendationParameters
+
+router = APIRouter()
+
+@router.post("/generate-recommedations/")
+async def get_recommendation(parameters: RecommendationParameters):
+    try:
+        recommendation = await fetch_recommedations(parameters)
+        return recommendation
+    except HTTPException as e:
+        raise HTTPException(
+            status_code=recommendation.status_code, detail=recommendation.json()
+        )
