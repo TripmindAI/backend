3017204adfd190966d3937548cd431a1b3548582
Chong Wen <chongwen107@gmail.com>
2024-03-04 00:05:18 -0600

    add search by db id

--- src/db/crud.py
+++ src/db/crud.py
@@ -3,12 +3,16 @@ from . import models
 from .. import schemas
 
 
-def get_location(db: Session, location_id: str):
+def get_location_by_location_id(db: Session, location_id: str):
     return (
         db.query(models.Location)
         .filter(models.Location.location_id == location_id)
         .first()
     )
+
+
+def get_location_by_id(db: Session, id: int):
+    return db.query(models.Location).filter(models.Location.id == id).first()
 
 
 def get_location_by_name(db: Session, name: str):
@@ -21,7 +25,7 @@ def get_locations(db: Session, skip: int = 0, limit: int = 10):
 
 
 def create_location(db: Session, location: schemas.LocationCreate):
-    db_location = models.Location(location.model_dump())
+    db_location = models.Location(**location.model_dump())
     db.add(db_location)
     db.commit()
     db.refresh(db_location)
