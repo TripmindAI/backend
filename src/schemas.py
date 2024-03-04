1d5d31a80205570bf037463c42d6e441cf959be1
Chong Wen <chongwen107@gmail.com>
2024-03-03 23:08:18 -0600

    remove required fields for lati and longti

--- src/schemas.py
+++ src/schemas.py
@@ -42,8 +42,8 @@ class Location(LocationBase):
     postalcode: str
 
     # Location data
-    latitude: float = Field(..., ge=-90, le=90)
-    longitude: float = Field(..., ge=-180, le=180)
+    latitude: float = Field(ge=-90, le=90)
+    longitude: float = Field(ge=-180, le=180)
     timezone: str
 
     @validator("timezone")
