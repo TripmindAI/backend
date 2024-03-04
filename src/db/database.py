0bd7a64b23b91f32fe4730ad89046191f47a60bc
Chong Wen <chongwen107@gmail.com>
2024-03-03 22:52:27 -0600

    add connection string

--- src/db/database.py
+++ src/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "postgresql://avnadmin:AVNS_I6x_zF_TX3Z1vMo3SE7@pg-dev-tripmind-develop.a.aivencloud.com:16222/defaultdb?sslmode=require"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
