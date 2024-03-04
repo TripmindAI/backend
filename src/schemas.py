906b7dbe68427342ac56577a5b34c2c8df3e9236
Chong Wen <chongwen107@gmail.com>
2024-03-03 21:35:11 -0600

    add validation for time zone and country

--- src/schemas.py
+++ src/schemas.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 import pycountry
+from zoneinfo import ZoneInfo, available_timezones
 
 
 class RecommendationParameters(BaseModel):
@@ -12,9 +13,15 @@ class LocationBase(BaseModel):
     name: str = Field(min_length=1, max_length=100)
     city: str = Field(min_length=3, max_length=30)
     state: str = Field(min_length=3, max_length=30)
-    country: str = Field(min_length=3, max_length=30)
-
-
+    country: str = Field(min_length=3, max_length=60)
+    
+    @validator("country")
+    def country_exists(cls, v):
+        try:
+            pycountry.countries.lookup(v)
+        except LookupError:
+            raise ValueError(f"{v} is not a valid ISO 3166 country code")
+        return v
 
 
 
@@ -27,6 +34,7 @@ class Location(LocationBase):
     location_id: str
     web_url: str
     is_active: bool
+    create_at: str
 
     # Address information
     street1: str
@@ -37,6 +45,12 @@ class Location(LocationBase):
     latitude: float = Field(..., ge=-90, le=90)
     longitude: float = Field(..., ge=-180, le=180)
     timezone: str
+
+    @validator("timezone")
+    def timezone_exists(cls, v):
+        if v not in available_timezones():
+            raise ValueError(f"{v} is not a valid timezone")
+        return v
 
     class Config:
         orm_mode = True
