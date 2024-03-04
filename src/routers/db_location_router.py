6cf6d337648c0fe9d188661710c3981da693b1f0
Chong Wen <chongwen107@gmail.com>
2024-03-03 21:26:56 -0600

    add api for adding location(Deveoloper Only)

---
+++ src/routers/db_location_router.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, HTTPException, status, Depends
+from .. import schemas
+from ..db import crud
+from sqlalchemy.orm import Session
+from ..db.database import SessionLocal
+
+router = APIRouter()
+
+
+# Dependency to get the database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/locations/", response_model=schemas.Location)
+def add_location(location: schemas.LocationCreate, db: Session = Depends(get_db)):
+    db_location = crud.create_location(db=db, location=location)
+    if db_location:
+        return db_location
+    raise HTTPException(status_code=400, detail="Location could not be created")
