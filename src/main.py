fb9dfd5fdecedef622cc89fc3c0003f569489567
Chong Wen <chongwen107@gmail.com>
2024-03-04 22:51:11 -0600

    improve restful paths

--- src/main.py
+++ src/main.py
@@ -5,6 +5,6 @@ from .routers import google_maps_router, openai_router, db_location_router
 app = FastAPI()
 
 #Include the routers from the routers folder
-app.include_router(openai_router.router, prefix="/backend-api/openai", tags=["OpenAI"])
-app.include_router(google_maps_router.router, prefix="/backend-api/google-maps", tags=["Google Maps"])
-app.include_router(db_location_router.router, prefix="/db-api", tags=["DB Opearations"])
+app.include_router(openai_router.router, prefix="/v1/backend-api/openai", tags=["OpenAI"])
+app.include_router(google_maps_router.router, prefix="/v1/backend-api/google-maps", tags=["Google Maps"])
+app.include_router(db_location_router.router, prefix="/v1/backend-api/locations", tags=["DB Opearations"])
