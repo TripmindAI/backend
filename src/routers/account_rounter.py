c1a98d9ded5429237a70911269c4c69e709b799e
XRFXRF <Felixxrf@outlook.com>
2024-03-04 23:51:48 -0600

    chaneg auth

--- src/routers/account_rounter.py
+++ src/routers/account_rounter.py
@@ -6,11 +6,14 @@ from ..dependencies.database_dependencies import get_db
 from ..db import crud
 
 router = APIRouter()
-@router.get("/auth/login", response_model=schemas.User)
+@router.get("/auth/login")
 async def user_login(claims: dict = Depends(decode_and_verify_token), db: Session = Depends(get_db)):
     try:
+        print(claims)
         user_info = parse_clains(claims)
-        db_user = crud.upsert_user(db, schemas.UserCreate(**user_info))
-        return db_user
+        print(user_info)
+        # db_user = crud.upsert_user(db, schemas.UserCreate(**user_info))
+        # return db_user
     except Exception as e:
+        print(e)
         raise HTTPException(status_code=400, detail=str(e))
